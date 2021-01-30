e3a468b8b576f100ea8b64bbc546abfe1a9622e7
Fred Rocha <fredsrocha1985@gmail.com>
2021-01-29 23:17:55 -0300

    Create Source

---
+++ source/app/admin/assets/js/script.js
@@ -0,0 +1,1 @@
+$(".checkout").click(function(){"../../assets/img/available.png"==$(this).attr("src")?$(this).attr("src","../../assets/img/unavailable.png"):$(this).attr("src","../../assets/img/available.png")}),$(".forgetfulness").click(function(){alert()});
